6a3f3e7d09ed14f91b762e8c1f630297ed1bcf41
haige1234 <lijianghai3@h-partners.com>
2026-01-06 15:22:33 +0800

    updata 3.6.5
    Signed-off-by: haige1234 <lijianghai3@h-partners.com>

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -1429,7 +1429,7 @@
  * Module:  library/psa_crypto.c
  * Requires: MBEDTLS_PSA_CRYPTO_C
  */
-//#define MBEDTLS_PSA_KEY_STORE_DYNAMIC
+#define MBEDTLS_PSA_KEY_STORE_DYNAMIC
 
 /**
  * Uncomment to enable p256-m. This is an alternative implementation of
@@ -2755,7 +2755,7 @@
  *
  * This module provides debugging functions.
  */
-//#define MBEDTLS_DEBUG_C
+#define MBEDTLS_DEBUG_C
 
 /**
  * \def MBEDTLS_DES_C
@@ -3251,7 +3251,7 @@
  *
  * This module enables PKCS#12 functions.
  */
-//#define MBEDTLS_PKCS12_C
+#define MBEDTLS_PKCS12_C
 
 /**
  * \def MBEDTLS_PLATFORM_C
